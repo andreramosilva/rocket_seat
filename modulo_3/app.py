1e9747236c82fbc513b62c2e30a1f3513e3dad96
asramos_meli <andre.sramos@mercadolivre.com>
2024-08-28 21:06:25 -0300

    modulo3

---
+++ modulo_3/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from models.task import Task
+
+# __name__ == __main__
+tasks = []
+task_id_control = 1
+app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "hello"
+
+@app.route("/tasks",methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control,title=data.get("title"),description=data.get("description", ""))
+    task_id_control+=1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"mensagem" : "Nova tarefa criada com sucesso"}) 
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    # for task in tasks:
+    #     task_list.append(task.to_dict())
+
+    result = { "tasks": task_list,
+              "total_tasks": len(task_list)
+    }    
+    return jsonify(result) 
+
+@app.route("/tasks/<int>:id",methods=["GET"])
+def get_task(id):
+    task=None
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message":"Nao foi possivel encontrar a atividade"}),404
+
+if __name__ == "__main__":
+    app.run(debug=True)
