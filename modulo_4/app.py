9d1ad1d8a590909e9f69c580c59597773e77e1f5
asramos_meli <andre.sramos@mercadolivre.com>
2024-08-30 21:51:36 -0300

    auth 1 modulo 4
    exit()
    :wq
    quit
    q

--- modulo_4/app.py
+++ modulo_4/app.py
@@ -1,10 +1,36 @@
 from flask import Flask, request, jsonify
+from models.user import User
 from database import db
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mysecretkey"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+# view login
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    senha = data.get('senha')
+
+    if username and senha:
+        user = User.query.filter_by(username=username).first()
+
+        if user is not None and user.senha == senha:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({'message': 'User authenticated'})
+    
+    return jsonify({'message': 'User not authenticated'}), 401
 
 @app.route('/hello', methods=['GET'])
 def hello():
--- modulo_4/models/user.py
+++ modulo_4/models/user.py
@@ -1,5 +1,5 @@
-from app import db
-from database import UserMixin
+from database import db
+from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
