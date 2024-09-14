af0e24b6ad2f11cb43fa00321354c2ed7e508c9c
asramos_meli <andre.sramos@mercadolivre.com>
2024-09-14 18:39:54 -0300

    calculadora 2 interface testes

--- Modulo_6/PrimeiraCalculadora/src/calculators/calculator_2_test.py
+++ Modulo_6/PrimeiraCalculadora/src/calculators/calculator_2_test.py
@@ -1,19 +1,35 @@
 from src.calculators.calculator_2 import Calculator2
-from typing import Dict
+from typing import Dict, List
 from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_hander_interface import DriverHandlerInterface
+
 
 class MockRequest:
     def __init__(self, body: Dict)->None    :
         self.json = body
+    
+class MockDriverHandler(DriverHandlerInterface):
+    def standard_deviation(self, data: List[float])->float:
+        return 0.14
 
-def test_calculate():
+# integraçao entre numpyhandler e calculator2
+def test_calculate_integration():
     mock_request = MockRequest({"numbers": [1, 2, 3]})
 
     driver = NumpyHandler()
     calculator = Calculator2(driver)
     result = calculator.calculate(mock_request)
-    
+
     assert isinstance(result, dict)
     assert result == { "data": { "calculator": 2, "result": 0.14 }}
     
     
+def test_calculate():
+    mock_request = MockRequest({"numbers": [1, 2, 3]})
+
+    driver = MockDriverHandler()
+    calculator = Calculator2(driver)
+    result = calculator.calculate(mock_request)
+
+    assert isinstance(result, dict)
+    assert result == { "data": { "calculator": 2, "result": 7.14 }}
